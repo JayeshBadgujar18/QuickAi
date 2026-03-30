d4527620fe5f0a5ef2b20750806eea6e9653abcf
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-03-30 13:13:25 +0530

    feat: create Home page component with Navbar integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from '../componets/Navbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Navbar/>
+    
+    
+    </>
+   
   )
 }
 
