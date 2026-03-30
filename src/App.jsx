2d254dd60b39c4b3c15a284fbadc59c201e4ef1b
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-03-30 12:11:47 +0530

    feat: initialize application routing with Layout and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import ReviewResume from './pages/ReviewResume'
+import RemoveObject from './pages/RemoveObject'
+import Removebackground from './pages/Removebackground'
+import GenerateImages from './pages/GenerateImages'
+import Community from './pages/Community'
+
+
+
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+
+        <Route path='/ai' element={<Layout/>}>
+        <Route index element={<Dashboard/>} />
+        <Route path='WriteArticle' element={<WriteArticle/>} />
+        <Route path='BlogTitles' element={<BlogTitles/>} />
+        <Route path='ReviewResume' element={<ReviewResume/>} />
+        <Route path='RemoveObject' element={<RemoveObject/>} />
+        <Route path='Removebackground' element={<Removebackground/>} />
+        <Route path='GenerateImages' element={<GenerateImages/>} />
+        <Route path='Community' element={<Community/>} />
+
+        
+
+        </Route>
+          
+            
+       
+      </Routes>
+    </div>
   )
 }
 
