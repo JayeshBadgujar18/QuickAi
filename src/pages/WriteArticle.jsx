c31a327fb67ac2804f90712b5dcd2931bb2aecd9
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-04-02 11:42:31 +0530

    feat: add WriteArticle page component with layout structure

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const WriteArticle = () => {
   return (
-    <div>WriteArticle</div>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+   {/* left col */}
+   <form className='w-full p-4 max-w-lg bg-white rounded-lg border border-gray-200 shadow-sm'>
+
+   </form>
+
+   {/* right col */}
+
+   <div>
+
+
+   </div>
+
+
+    </div>
   )
 }
 
