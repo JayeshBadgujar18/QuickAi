3bb5a20851227f54713b7a48cef44ed74a7d1510
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-04-02 11:48:41 +0530

    feat: add WriteArticle page component with initial configuration form layout

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -1,3 +1,4 @@
+import { Sparkles } from 'lucide-react'
 import React from 'react'
 
 const WriteArticle = () => {
@@ -5,6 +6,12 @@ const WriteArticle = () => {
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
    {/* left col */}
    <form className='w-full p-4 max-w-lg bg-white rounded-lg border border-gray-200 shadow-sm'>
+
+    <div className='flex items-center gap-3'>
+
+      <Sparkles className='w-6 h-6 text-[#4A7AFF]'/>
+      <h1 className='text-xl font-semibold'> Article Configuration</h1>
+    </div>
 
    </form>
 
