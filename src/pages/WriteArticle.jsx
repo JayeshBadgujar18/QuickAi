eac4c7fd8dbe0d09821fe97e5595ff54926ffac9
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-04-03 01:28:39 +0530

    feat: add WriteArticle page with topic input and length configuration options

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -1,7 +1,19 @@
-import { Sparkles } from 'lucide-react'
+import { Sparkles, Edit } from 'lucide-react'
 import React from 'react'
+import { useState } from 'react'
 
 const WriteArticle = () => {
+
+  const articleLength = [
+    {length:800 ,text :'Short (500-800 words)'},
+    {length:1200 ,text :'Medium (800-1200 words)'},
+    {length:1600 ,text :'Long (1200-1600 words)'},
+  ]
+
+  const [selectedLength,setSelectedLength] = useState(articleLength[0].length)
+  const [input,setInput] = useState('')
+
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
    {/* left col */}
@@ -12,6 +24,26 @@ const WriteArticle = () => {
       <Sparkles className='w-6 h-6 text-[#4A7AFF]'/>
       <h1 className='text-xl font-semibold'> Article Configuration</h1>
     </div>
+    <p className='font-medium text-sm mt-6'>
+      Article Topic
+    </p>
+    <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='the future of artificial intelligience' className='w-full border border-gray-300 rounded-md px-3 p-2 mt-2 focus:outline-none focus:ring-2 focus:ring-primary outline-none text-sm' required />
+    
+    <p className='mt-4 font-medium text-sm'>Article Length</p>
+
+    <div className='flex flex-wrap gap-3 mt-3 sm:max-w-9/11'>
+      {articleLength.map((item,index)=>(
+       
+          <span key={index} onClick={()=>setSelectedLength(item.length)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedLength === item.length ? 'bg-primary text-white' : ''}`}>{item.text}</span>
+        
+      ))}
+    </div>
+    <br />
+    <button className=' w-full flex justify-center  items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 rounded-lg cursor-pointer'>
+      <Edit className='w-5 h-5 '/>
+      Generate Article
+    </button>
+
 
    </form>
 
