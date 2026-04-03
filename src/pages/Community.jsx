f02f89e3cd1ddad1903e5b32b91f8da5d4508bb6
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-04-03 22:42:00 +0530

    feat: add Community page to display published user creations

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useUser } from '@clerk/react'
+import {dummyPublishedCreationData} from '../assets/assets'
+
+
 
 const Community = () => {
+  const [creations  ,setcreations]=useState([])
+  const {user}=useUser();
+
+  const fetchCreations=async()=>{
+    setcreations(dummyPublishedCreationData)
+    
+  }
+  useEffect(()=>{
+    if(user){
+      fetchCreations()
+    }
+  },[user])
+
   return (
-    <div>Community</div>
+    <div className=''>
+      Creations
+      <div className='bg-white h-full w-full rounded-xl overflow-y-scroll'>
+
+        {creations.map((creation,index)=>(
+          <div key={index} className='relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3'>
+            <img src={creation.content} alt="" className='w-full h-full object-cover rounded-lg' />
+            <p>{creation.prompt}</p>
+          </div>
+        ))}
+
+
+      </div>
+
+
+    </div>
   )
 }
 
