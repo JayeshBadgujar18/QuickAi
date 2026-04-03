fed7d2bea695ff96e1ed353f6a28734515796a1f
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-04-03 23:00:58 +0530

    feat: add Community page to display and interact with published user creations(Complted FrontEnd)

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useUser } from '@clerk/react'
 import {dummyPublishedCreationData} from '../assets/assets'
+import { Heart } from 'lucide-react'
 
 
 
@@ -27,7 +28,15 @@ const Community = () => {
         {creations.map((creation,index)=>(
           <div key={index} className='relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3'>
             <img src={creation.content} alt="" className='w-full h-full object-cover rounded-lg' />
-            <p>{creation.prompt}</p>
+            <div className='absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg'>
+              <p className='text-sm hidden group-hover:block'>{creation.prompt}</p>
+              <div className='flex gap-1 items-center'>
+                <p>
+                  {creation.likes.length}
+                  <Heart className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${creation.likes.includes(user?.id) ? 'fill-red-500 text-red-600' : 'text-white'}`}/>
+                </p>
+              </div>
+            </div>
           </div>
         ))}
 
